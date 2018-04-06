223700996acc46f2914e59cbfc70e5a115635557
tomonarifeehan <feehan.tomonari@gmail.com>
2018-04-06 17:22:56 -0400

    Updated screenshot code

--- bustabit.py
+++ bustabit.py
@@ -1,23 +1,11 @@
 from PIL import Image
 from pytesser import image_to_string
 import pyscreenshot as ImageGrab
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import time
 import sys
 
-url = 'https://www.bustabit.com/play'
 
-chrome_options = webdriver.ChromeOptions()
-prefs = {"profile.default_content_setting_values.notifications" : 2}
-chrome_options.add_experimental_option("prefs",prefs)
-driver = webdriver.Chrome(chrome_options=chrome_options)
-
-
-
-#Navigate to URL
-
-# part of the screen
+# Captures Screenshot
 if __name__ == "__main__":
 	driver.get(url)
 	time.sleep(5)
@@ -33,7 +21,7 @@ if __name__ == "__main__":
 
 	counter = counter + 1
 	for num in range(counter):
-		im = Image.open("C:\Users\Tomonari\workspace\python\Bustabit" + "\im" + str(counter) + ".png")
+		im = Image.open("C:\Users\Tomonari\Desktop\seizureDection" + "\im" + str(counter) + ".png")
 		text = image_to_string(im.convert('RGB'))
 		
 		busted = text.strip()
